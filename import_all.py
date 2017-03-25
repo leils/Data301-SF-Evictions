a87386d1eb26d70f1d94410d16dc25083384b8d3
Leia <e.leiachang@gmail.com>
2017-03-25 13:16:06 -0700

    Adding files

---
+++ import_all.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np 
+
+#rent = pd.read_csv('/data/leils/tidy_rent_increase.csv')
+evict = pd.read_csv('/data/leils/tidy_evictions.csv')
+police = pd.read_csv('/data/leils/tidy_police.csv')
